a9f2b884d72db284028fc003aa50c94fdd379c7e
Fatin Ishraque <155258978+n0vemberain@users.noreply.github.com>
2025-10-31 23:20:21 +0600

    Dom basic

--- Module 25/README.md
+++ Module 25/README.md
@@ -1,1 +1,10 @@
 DOM -> Document object model <br>
+why DOM required? -> Dynamic content updates, whithout reloading the page the contents updates. <dr>
+it makes the web user interactive <br>
+realtime modification opportunity <br>
+
+DOM data type : <br>
+Document : Think of an entire web page. <br>
+Node : Anything on the web page <br>
+Elements : Specific types of node . it represents actual tags. <br>
+NodeList : Group of nodes stored together. <br>
--- Module 25/dom.js
+++ Module 25/dom.js
@@ -1,2 +1,34 @@
-console.log("Hello form dom")
-console.log(document);
+const sections  = document.querySelectorAll('section');
+// console.log(sections);
+
+for (const section of sections){
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '10px';
+    section.style.padding = '10px';
+    section.style.borderRadius = '10px';
+    section.style.backgroundColor = 'lightgray';
+
+}
+
+const placesContainer = document.getElementById('places-container');
+// placesContainer.style.backgroundColor = 'lightblue';
+// placesContainer.style.padding = '20px';
+// placesContainer.style.borderRadius = '10px';
+// placesContainer.style.marginBottom = '10px';
+placesContainer.classList.add('okay');
+
+
+
+const section = document.createElement('section');
+// const h1 = document.createElement('h1');
+// h1.innerText = 'New Section Added';
+// section.appendChild(h1);
+
+// mainContainer.appendChild(section);
+
+section.innerHTML = `
+    <h1>New Section Added</h1>
+    <p>This is a new section added using JavaScript.</p>
+`;
+
+mainContainer.appendChild(section);
